240d283aa93168a156a650bc30a0944f0cf16a31
CoBr <cole.broughton@gmail.com>
2019-05-04 20:29:52 -0700

    physical optics through linear algebra

---
+++ optics/LA-wrt-PhysOptics.py
@@ -0,0 +1,7 @@
+## Linear Algebra with application to physical optics
+
+import numpy as np
+import sympy as sp
+import astropy.units as u
+from numpy import sin,cos,pi,sqrt
+import matplotlib.pyplot as plt
